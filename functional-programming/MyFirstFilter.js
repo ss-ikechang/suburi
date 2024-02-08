499ca2342c431c6b156d97d6d07f83ab38835699
shunsuke ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-02-08 11:32:40 +0900

    My First Filter

--- functional-programming/MyFirstFilter.js
+++ functional-programming/MyFirstFilter.js
@@ -1,1 +1,10 @@
+const isEven = function(value) {
+    if (value % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+const even = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(isEven);
+console.log('even = ' + even);
