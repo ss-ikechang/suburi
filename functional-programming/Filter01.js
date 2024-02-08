2eb822b1cadaa04b10b2dab75a2cba551de7cf7f
shunsuke ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-02-08 11:41:00 +0900

    filter実装

--- functional-programming/Filter01.js
+++ functional-programming/Filter01.js
@@ -1,1 +1,11 @@
+const isEven = function(element, index, array)  {
+    console.log("現在の値：" + element + ", インデックス：" + index + ", 配列：" + array);     
+    if (element % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+const even = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(isEven);
+console.log('even = ' + even);
