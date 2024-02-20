6b88ff382cf2fba10dbefe57b3faf13cbc1c74a0
Shunsuke Ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-02-21 06:19:02 +0900

    Mapクロージャーの一番単純な形

---
+++ functional-programming/Closures/Map05.js
@@ -0,0 +1,8 @@
+function main() {
+    for (let x = 1; x <= 9; x++) {
+        // Functions declared within loops referencing an outer scoped variable may lead to confusing semantics. (x)
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(num => num * x );
+        console.log('numbers = ' + numbers);
+    }
+}
+main();
