d8524133c15f1f544675f5ccec5fd07f04dce23d
shunsuke ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-03-28 20:03:38 +0900

    コメント付与

--- functional-programming/Closures/Closure01.js
+++ functional-programming/Closures/Closure01.js
@@ -1,15 +1,21 @@
+// [JavaScript]クロージャについての現在の理解 #JavaScript - Qiita
+// https://qiita.com/maccotsan/items/4a31bdcfc649a972608e
+
+// クロージャーをJavaScriptで理解する・使い方 | QooTechWEB
+// https://engineering.webstudio168.jp/2022/03/javascript-closure/
+
 function closure(initVal) {
-    let count = initVal;
-    
-    return () => ++count;
-    // let innerFunc = () => ++count;
-    // let innerFunc = () => {
-    //     return ++count;
-    // }
-    // let innerFunc = function() {
-    //     return ++count;
-    // }
-    // return innerFunc;
+  let count = initVal;
+
+  //   return () => ++count;
+  // let innerFunc = () => ++count;
+  // let innerFunc = () => {
+  //     return ++count;
+  // }
+  let innerFunc = function () {
+    return ++count;
+  };
+  return innerFunc;
 }
 
 let myClusure1 = closure(100);
